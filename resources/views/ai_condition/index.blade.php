d45101556eced54c6854bce021d89c0697097805
Mahen <mahennekkers27@gmail.com>
2024-10-21 19:26:18 +0700

    refactor: finished first ui ai plant condition page

--- resources/views/ai_condition/index.blade.php
+++ resources/views/ai_condition/index.blade.php
@@ -20,15 +20,27 @@
 <body>
     <div class="flex w-full">
         <!-- Sidebar -->
-        <div class="w-1/5">
+        <div class="w-1/5 h-screen">
             @include('layouts.sidebar')
         </div>
     
         <!-- PLant Condition -->
-        <div class="w-4/5">
-            <div class="condtion-container w-full bg-light-green h-screen p-5">
-                This is content of plant condition
-            </div>
+        <div class="w-4/5 h-screen">
+            <header class="flex bg-light-green text-dark-green shadow-md rounded-lg p-6 gap-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg>
+                <h1>AI Kondisi Tanaman (Generate AI Kondisi Daun dan Buah Cabai) adalah sebuah fitur berbasis IoT dan AI yang dirancang untuk menganalisis kesehatan daun dan buah cabai secara otomatis menggunakan kamera dan model AI yang telah dilatih. Fitur ini dapat membantu petani atau pengguna untuk mengetahui kondisi tanaman mereka secara real-time dan memberikan rekomendasi tindakan berdasarkan hasil analisis gambar.</h1>
+            </header>
+            <main class="main-container w-full p-5 gap-3 justify-center mt-3">
+                <button class="capture-container flex gap-3 justify-center w-full text-white bg-dark-green p-5 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
+                    </svg>
+                    <h1>Ambil Foto Tanaman</h1>  
+                </button>
+            </main>
         </div>
     </div>
 </body>
