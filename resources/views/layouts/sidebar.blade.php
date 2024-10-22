d9d0004d1b3484e6de8a11f17114e421dc34899b
Mahen <mahennekkers27@gmail.com>
2024-10-22 09:51:47 +0700

    style: change active color sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -31,7 +31,7 @@
             </svg>
             <h1>Dashboard</h1>
         </a>
-        <a href="{{ route('ai.index') }}" class="flex gap-2 p-3 rounded-lg {{ request()->routeIs('ai.index') ? 'bg-white text-black': '' }}">
+        <a href="{{ route('ai.index') }}" class="flex gap-2 p-3 rounded-lg {{ request()->routeIs('ai.index') ? 'bg-light-green text-black': '' }}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
@@ -39,7 +39,7 @@
             <h1>AI Kondisi Tanaman</h1>
         </a>
         
-        <a href="{{ route('schedule.index') }}" class="flex gap-2 p-3 rounded-lg  {{ request()->routeIs('schedule.index') ? 'bg-white text-black': '' }}">
+        <a href="{{ route('schedule.index') }}" class="flex gap-2 p-3 rounded-lg  {{ request()->routeIs('schedule.index') ? 'bg-light-green text-black': '' }}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
